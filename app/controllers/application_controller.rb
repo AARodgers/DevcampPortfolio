963f2c24ba42795c1adb6ef0d0a5383c41913e61
Amanda Rodgers <aarodgerswork@gmail.com>
2022-04-07 17:03:48 -0600

    added controller concerns for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_filter means before you run any other controller because all inherit from this one, run the
-  # :configure_permitted_parameters method but only if there is a devise controller action
-  # i want to create a method and have it run anytime your communicating with devise
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    # these are devise methods
-  end
+  # to include this module, is better practice to include a module here than put all of the logic/methods here
+  include DeviseWhitelist
 end
