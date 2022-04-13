93aa134dce98c0fe72487617e61788bd12d83e0c
Amanda Rodgers <aarodgerswork@gmail.com>
2022-04-13 13:47:01 -0600

    Added custom layouts for blog and portfolio

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy toggle_status ]
+  layout "blog"
+  # tells what layout/stylesheet to use, layout is a method and "blog" is the arguement to that method
 
   # GET /blogs or /blogs.json
   def index
